41321b6ea6220bbfb032a709e885e8e45530cc0b
Chaldean World Builder <builder@chaldean.world>
2025-12-01 22:33:08 -0500

    Add scroll to top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Lessons from './pages/Lessons';
@@ -7,10 +8,24 @@ import Practice from './pages/Practice';
 import Dictionary from './pages/Dictionary';
 import { ProgressProvider } from './context/ProgressContext';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const mainElement = document.querySelector('main');
+    if (mainElement) {
+      mainElement.scrollTo(0, 0);
+    }
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <ProgressProvider>
       <HashRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
