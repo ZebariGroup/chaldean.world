c4d1399cd16394d0d950f32244b228f5678ea63c
Supabase User <supabase@chaldean.world>
2025-12-04 14:12:44 -0500

    Remove unused useAuth import from Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import { useProgress } from '../context/ProgressContext';
-import { useAuth } from '../context/AuthContext';
 import { dictionaryData } from '../data/dictionary';
 import { useMemo } from 'react';
 
